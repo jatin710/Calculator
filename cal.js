be1fb21c5bdc7a69c2a102873a5a3b4130921ba4
jatin710 <47669092+jatin710@users.noreply.github.com>
2020-12-29 18:55:40 +0530

    Add files via upload

---
+++ cal.js
@@ -0,0 +1,33 @@
+let screen = document.getElementById('screen');
+buttons = document.querySelectorAll('button');
+let screenValue=" ";
+for( item of buttons)  {
+    item.addEventListener('click', (e)=> {
+        buttonText = e.target.innerText;
+        console.log("button text is", buttonText);
+        if( buttonText =='X')  {
+            buttonText="*";
+            screenValue += buttonText;
+            screen.value = screenValue;
+
+        }
+       else if( buttonText == '=')
+    {
+        screen.value = eval(screenValue);
+        screenValue = screen.value;
+    }
+    else if( buttonText == 'AC')
+    {
+        screenValue = " ";
+        screen.value = screenValue;
+    }
+    else 
+    {
+      screenValue +=  buttonText;
+      screen.value =  screenValue;
+    }
+    })
+    
+    
+
+}
